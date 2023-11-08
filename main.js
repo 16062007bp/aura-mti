7df6324fe127d8ae68b53889c1a3bad4873585df
16062007bp <106752255+16062007bp@users.noreply.github.com>
2023-11-08 11:27:25 -0300

    bidas

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-function tocaSomPom(){ 
-    docement.quertysector(#som_tecla_pom).play();
+//criando/declarando a função tocaSom com parâmetro idElementoAudio
+unction tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-docement.quertysector(#som_tecla_pom).play();
+//criando a referência constante listaDeTeclas
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//criando referência variável contador, atribuindo o valor inicial como "0"
+let contador = 0;
+//Criando o código de repetição WHILE
+//enquanto o contador for menos que o comprimento da lista, tocará o som. 11 while (contador < listaDeTeclas. Length)
+const tecla= listaDeTeclas [contador]
+const efeito = tecla.classList[1];
+const idAudio = "#som "+efeito;
+listaDeTeclascontrador
+conts idAudio= `#som_$ efeito`;
+tecla.
+tecla.onclick = function(){}
+    tocaSom (idAudio);
+    contador contador + 1;
+    console.log(contador);
+    {
+
+}
